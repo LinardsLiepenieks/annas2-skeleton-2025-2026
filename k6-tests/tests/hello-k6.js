32f5b6e7ec6357f7edcd4ba3e9c1b25cadc96786
LinardsLiepenieks <49512889+LinardsLiepenieks@users.noreply.github.com>
2025-10-07 17:42:13 +0000

    scalability fundamentals

--- k6-tests/tests/hello-k6.js
+++ k6-tests/tests/hello-k6.js
@@ -15,17 +15,30 @@ export const options = {
         },
       },
     },
-    /*server: {
+    server: {
       vus: 2,
       duration: '10s',
       executor: 'constant-vus',
       exec: 'getServerRoot',
-    },*/
+    },
   },
 };
 
 export const loadPage = async () => {
   const page = await browser.newPage();
 
-  await page.goto('http://client:4321');
+  try {
+    await page.goto('http://client:4321');
+  } 
+  finally {
+    await page.close();
+  }
 };
+
+
+export const getServerRoot = async () => {
+  const url = "http://server:8000/";
+  const res = http.get(url);
+
+  check(res, {"status is 200": (r) => r.status === 200})
+}
