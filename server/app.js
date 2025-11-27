f2a318e68b718147d27312ce8788229398fdc495
LinardsLiepenieks <49512889+LinardsLiepenieks@users.noreply.github.com>
2025-11-27 17:40:47 +0000

    docker issues

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import { Hono } from '@hono/hono';
 import { cors } from '@hono/hono/cors';
 import { logger } from '@hono/hono/logger';
 import postgres from "postgres";
+import { cache } from "@hono/hono/cache";
 
 const app = new Hono();
 const sql = postgres();
@@ -11,8 +12,19 @@ app.use('/*', logger());
 
 app.get('/', (c) => c.json({ message: 'Hello world!' }));
 app.get("/todos", async (c) => {
+    
     const todos = await sql`SELECT * FROM todos`;
     return c.json(todos)
+})
+
+app.get("/cache-demo", cache({
+    cacheName: "demo-cache",
+    wait: true,
+}))
+
+app.get("/cache-demo", async (c) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return c.json({message: "Cache demo2"});
 })
 
 export default app;
