39bbd2d0a911ee8b1adb0c5882779f01d8a36e70
LinardsLiepenieks <linardsliepenieks@gmail.com>
2025-12-02 19:37:51 +0200

    caching demo

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 import { Hono } from '@hono/hono';
 import { cors } from '@hono/hono/cors';
 import { logger } from '@hono/hono/logger';
-import postgres from "postgres";
-import { cache } from "@hono/hono/cache";
+import postgres from 'postgres';
+import { cache } from '@hono/hono/cache';
 
 const app = new Hono();
 const sql = postgres();
@@ -11,20 +11,39 @@ app.use('/*', cors());
 app.use('/*', logger());
 
 app.get('/', (c) => c.json({ message: 'Hello world!' }));
-app.get("/todos", async (c) => {
-    
-    const todos = await sql`SELECT * FROM todos`;
-    return c.json(todos)
-})
+app.get('/todos', async (c) => {
+  const todos = await sql`SELECT * FROM todos`;
+  return c.json(todos);
+});
 
-app.get("/cache-demo", cache({
-    cacheName: "demo-cache",
+app.get(
+  '/cache-demo',
+  cache({
+    cacheName: 'demo-cache',
     wait: true,
-}))
+  })
+);
 
-app.get("/cache-demo", async (c) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return c.json({message: "Cache demo2"});
-})
+app.get('/cache-demo', async (c) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return c.json({ message: 'Cache demo3' });
+});
+
+app.get(
+  '/hello/*',
+  cache({
+    cacheName: 'hello-cache',
+    wait: true,
+  })
+);
+
+app.get('/hello/:name', async (c) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return c.json({ message: `Hello ${c.req.param('name')}` });
+});
+app.post('/hello', async (c) => {
+  await caches.delete('hello-cache');
+  return c.json({ message: 'Hello cache cleared' });
+});
 
 export default app;
