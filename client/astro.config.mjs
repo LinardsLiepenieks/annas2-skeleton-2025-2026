bbe7ccf821a6d6282782785155f83028a0d9e4fc
Linards Liepenieks <linardsliepenieks@gmail.com>
2026-03-31 19:38:24 +0300

    back - mar31

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import deno from "@deno/astro-adapter";
+import svelte from "@astrojs/svelte"
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: deno(),
+    integrations: [svelte(), mdx()]
+});
